96f40c3fc91e3cd14721c433006bdc949daff462
Stephen Callen <src3363@rit.edu>
2017-12-04 03:07:48 -0500

    Something wrong with static variables in MyOctant.cpp

--- A06 - Octree/MyOctant.cpp
+++ A06 - Octree/MyOctant.cpp
@@ -1,6 +1,10 @@
 #include "MyOctant.h"
 using namespace Simplex;
 
+uint MyOctant::m_uMyOctantCount;
+uint MyOctant::m_uMaxLevel;
+uint MyOctant::m_uIdealEntityCount;
+
 Simplex::MyOctant::MyOctant(uint a_nMaxLevel, uint a_nIdealEntityCount)
 {
 	
@@ -32,13 +36,21 @@ Simplex::MyOctant::MyOctant(uint a_nMaxLevel, uint a_nIdealEntityCount)
 	}
 
 	m_v3Center = m_v3Max - m_v3Min;
-
+	m_fSize = m_v3Max.x - m_v3Min.x;
 	m_pRoot = this;
+
+	ConstructTree(a_nMaxLevel);
 }
 
 Simplex::MyOctant::MyOctant(vector3 a_v3Center, float a_fSize)
 {
+	m_uID = m_uMyOctantCount;
 	m_uMyOctantCount++;
+
+	m_v3Max = a_v3Center + vector3(a_fSize);
+	m_v3Min = a_v3Center - vector3(a_fSize);
+	m_v3Center = a_v3Center;
+	m_fSize = a_fSize;
 }
 
 Simplex::MyOctant::MyOctant(MyOctant const & other)
@@ -47,11 +59,22 @@ Simplex::MyOctant::MyOctant(MyOctant const & other)
 
 MyOctant & Simplex::MyOctant::operator=(MyOctant const & other)
 {
-	// TODO: insert return statement here
+	return *this;
 }
 
 Simplex::MyOctant::~MyOctant(void)
 {
+	m_pMeshMngr = nullptr;
+	m_pEntityMngr = nullptr;
+	delete(m_pParent);
+	m_pParent = nullptr;
+	delete[](m_pChild);
+	delete(m_pRoot);
+	m_pRoot = nullptr;
+	for (int i = 0; i < m_lChild.size(); i++) {
+		delete(m_lChild[i]);
+		m_lChild[i] = nullptr;
+	}
 }
 
 void Simplex::MyOctant::Swap(MyOctant & other)
@@ -80,15 +103,25 @@ vector3 Simplex::MyOctant::GetMaxGlobal(void)
 
 bool Simplex::MyOctant::IsColliding(uint a_uRBIndex)
 {
+	if((m_pEntityMngr->GetEntity(a_uRBIndex)->GetPosition().x >= m_v3Min.x && m_pEntityMngr->GetEntity(a_uRBIndex)->GetPosition().x < m_v3Max.x) &&
+		(m_pEntityMngr->GetEntity(a_uRBIndex)->GetPosition().y >= m_v3Min.y && m_pEntityMngr->GetEntity(a_uRBIndex)->GetPosition().y < m_v3Max.y) &&
+		(m_pEntityMngr->GetEntity(a_uRBIndex)->GetPosition().z >= m_v3Min.z && m_pEntityMngr->GetEntity(a_uRBIndex)->GetPosition().z < m_v3Max.z))
+		return true;
 	return false;
 }
 
 void Simplex::MyOctant::Display(uint a_nIndex, vector3 a_v3Color)
 {
+	m_pMeshMngr->AddCubeToRenderList(glm::translate(m_v3Center)* glm::scale(vector3(m_fSize)), a_v3Color, 1);
 }
 
 void Simplex::MyOctant::Display(vector3 a_v3Color)
 {
+	m_pMeshMngr->AddCubeToRenderList(glm::translate(m_v3Center) * glm::scale(vector3(m_fSize)), a_v3Color, 1);
+	if (m_uChildren != 0) {
+		for (int i = 0; i < 8; i++)
+			m_pChild[i]->Display();
+	}
 }
 
 void Simplex::MyOctant::DisplayLeafs(vector3 a_v3Color)
@@ -102,6 +135,30 @@ void Simplex::MyOctant::ClearEntityList(void)
 
 void Simplex::MyOctant::Subdivide(void)
 {
+	/*
+		   3-----0
+		  /     /|
+		 2-----1 |
+		 | 7   | 4
+		 |/    |/
+		 6-----5
+	*/
+
+	m_pChild[0] = new MyOctant(m_v3Max - vector3(m_fSize / 4.0f), m_fSize / 2.0f);
+	m_pChild[1] = new MyOctant(m_v3Max - vector3(m_fSize / 4.0f,m_fSize / 4.0f, 3.0f * m_fSize / 4.0f), m_fSize / 2.0f);
+	m_pChild[2] = new MyOctant(m_v3Max - vector3(3.0f * m_fSize / 4.0f, m_fSize / 4.0f, 3.0f * m_fSize / 4.0f), m_fSize / 2.0f);
+	m_pChild[3] = new MyOctant(m_v3Max - vector3(3.0f * m_fSize / 4.0f, m_fSize / 4.0f, m_fSize / 4.0f), m_fSize / 2.0f);
+
+	m_pChild[4] = new MyOctant(m_v3Max - vector3(m_fSize / 4.0f, 3.0f * m_fSize / 4.0f, m_fSize / 4.0f), m_fSize / 2.0f);
+	m_pChild[5] = new MyOctant(m_v3Max - vector3(m_fSize / 4.0f, 3.0f * m_fSize / 4.0f, 3.0f * m_fSize / 4.0f), m_fSize / 2.0f);
+	m_pChild[6] = new MyOctant(m_v3Max - vector3(3.0f * m_fSize / 4.0f, 3.0f * m_fSize / 4.0f, 3.0f * m_fSize / 4.0f), m_fSize / 2.0f);
+	m_pChild[7] = new MyOctant(m_v3Max - vector3(3.0f * m_fSize / 4.0f, 3.0f * m_fSize / 4.0f, m_fSize / 4.0f), m_fSize / 2.0f);
+
+	for (int i = 0; i < 8; i++) {
+		m_pChild[i]->m_pParent = this;
+		m_uChildren++;
+	}
+
 }
 
 MyOctant * Simplex::MyOctant::GetChild(uint a_nChild)
@@ -141,6 +198,11 @@ void Simplex::MyOctant::KillBranches(void)
 
 void Simplex::MyOctant::ConstructTree(uint a_nMaxLevel)
 {
+	if (a_nMaxLevel <= 0) {
+		return;
+	}
+	Subdivide();
+	ConstructTree(a_nMaxLevel--);
 }
 
 void Simplex::MyOctant::AssignIDtoEntity(void)
